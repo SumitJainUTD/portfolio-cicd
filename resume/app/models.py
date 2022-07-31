d074b035e246bef758148b68ba8ccbc6b46f9c96
SumitJainUTD <sumi12june@gmail.com>
2022-07-31 11:52:31 -0500

    added roku added display id in db for jobs

--- resume/app/models.py
+++ resume/app/models.py
@@ -4,9 +4,13 @@ from django.db import models
 # Create your models here.
 class Tile(models.Model):
     image = models.ImageField(upload_to='images/')
+    display_id = models.IntegerField(blank=True, default=1)
     title = models.CharField(max_length=50, blank=True)
     company = models.CharField(max_length=50, blank=True)
     detail = models.TextField(max_length=1000, blank=True)
+
+    class Meta:
+        ordering = ('display_id',)
 
 
 class Skill(models.Model):
